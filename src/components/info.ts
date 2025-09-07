0c00107823d33c5c2211e452fc1012a6d203b198
juanintriago <juanjoitoshiki@gmail.com>
2025-09-06 19:45:18 -0500

    feat: add logo and fix favicon and SEO

---
+++ src/components/info.ts
@@ -0,0 +1,17 @@
+export const info = {
+    title: "Gateway Academic",
+    logoUrl: "/logo.png",
+    description: "Institución educativa que transforma vidas a través de programas de inglés y certificaciones internacionales.",
+    keywords: "Educación, Inglés, Certificaciones, Gateway Academic, Programas de Inglés, Cambridge, TOEFL, IELTS",
+    promoImages :['/promo1.jpg', '/promo2.jpg', '/promo3.jpg'],
+    socialMediaLinks: {
+        facebook: "https://www.facebook.com/gatewayacademic",
+        instagram: "https://www.instagram.com/gatewayacademic",
+        linkedin: "https://www.linkedin.com/company/gatewayacademic",
+        tiktok: "https://www.tiktok.com/@gatewayacademic",
+    },
+    contactEmail: "info@gatewayacademic.com",
+    contactPhone: "+",
+    address: "123 Main St, City, Country",
+    operatingHours: "Lunes a Viernes: 9am - 6pm",
+}

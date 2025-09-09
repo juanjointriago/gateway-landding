f27bfd4917c0231949692b6fad9d6dd4217c1837
juanintriago <juanjoitoshiki@gmail.com>
2025-09-09 08:41:12 -0500

    fix: mock fdata

--- src/components/info.ts
+++ src/components/info.ts
@@ -24,4 +24,13 @@ export const info = {
     },
     footerText: "© 2024 Gateway Academic. Todos los derechos reservados.",
     googleMapLink: "https://www.google.com/maps/dir/?api=1&destination=0.3497122865869%2C-78.117164969444",
+    mission:"Proporcionar a nuestros usuarios un servicio de óptima calidad que les permita desarrollar sus habilidades idiomáticas para competir con éxito en  un mundo cada día más formativo de rendimiento y productividad. El cambio será permanente… la evolución tecnológica y humana será constante.",
+    vision:"Consolidaremos nuestra presencia en el mercado mediante una  aptitud  de  servicio  y  compromiso permanente  con nuestra gente, alcanzando el nivel más alto en eficacia y satisfacción. En un futuro próximo seremos la empresa con más alto perfil en el área de enseñanza y aprendizaje delidioma Inglés en América Latina.",
+    values:[
+        {title: "Excelencia", description: "Comprometernos a ofrecer la mejor calidad en la enseñanza en todos nuestros programas, buscando siempre mejorar y superar las expectativas."},
+        {title: "Inclusividad", description: "Fomentar un ambiente donde todos se sientan bienvenidos, sin importar su origen, nivel de habilidad o experiencia previa. Celebrar la diversidad en el aprendizaje."},
+        {title: "Innovación", description: "Estar abiertos a nuevas ideas y métodos de enseñanza, utilizando tecnología y enfoques creativos para hacer que el aprendizaje sea más efectivo y atractivo."},
+        {title: "Compromiso", description: "Mantener un fuerte compromiso con el crecimiento personal y académico de nuestros alumnos, apoyándolos en su viaje hacia el dominio del inglés."},
+
+    ],
 }
